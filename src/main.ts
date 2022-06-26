cf76b6da530ac10bad5942cdafe6eef097a1cfac
Trijit Goswami <trijit@Trijits-MacBook-Air.local>
2022-06-26 11:37:40 +0530

    port issue fixed

--- src/main.ts
+++ src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
 
   app.useGlobalFilters(new HttpExceptionFilter());
 
-  await app.listen(3000 || process.env.PORT);
+  await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
